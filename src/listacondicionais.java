0bdfb334e01d69624ff50df0eae6ace8e6a5f7c9
Marcelo Schlemper <marcelo63466446@alunos.sc.senac.br>
2026-04-02 15:05:28 -0300

    exercicios 1, 2 ,3 e 4

--- src/listacondicionais.java
+++ src/listacondicionais.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class listacondicionais {
 
 	public static void main(String[] args) {
-    exercicio1();
-		
+    //exercicio1();
+	//exercicio2();
+	//exercicio3();
+	exercicio4();
 	}
 	
 	static void exercicio1() {
@@ -24,6 +26,53 @@ public class listacondicionais {
 	}
 	
 	static void exercicio2() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("digite o primeiro numero");
+		int numero1 = sc.nextInt();
+		
+		System.out.print("digite o segundo numero");
+		int numero2 =  sc.nextInt();
+		
+		if (numero1 > numero2 ) {
+		    System.out.println("o primeiro numero é maior!");
+        } else { 
+		   System.out.println("o segundo numero é maior!"); 
+        }	
+		sc.close();
+		
+		
+	}
+	
+	static void exercicio3() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("digite um numero");
+		int numero = sc.nextInt();
+		
+		if (numero > 0) {
+			System.out.println("o numero é positivo!");
+		} else if (numero < 0) {
+			System.out.println ("o numero é negativo");
+		} else 
+			System.out.println("o numero é zero!");
+		sc.close();
+		
+	}
+	
+	static void exercicio4() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("qual foi sua nota");
+		float nota = sc.nextFloat();
+		 if (nota < 7) {
+			 System.out.println("voce foi reprovado");
+		 } else {
+			 System.out.println("voce foi aprovado");
+			 
+         sc.close();
+		 }
+		
 		
 	}
 
