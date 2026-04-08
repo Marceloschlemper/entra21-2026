5edd2e84203461de2e8aa2a76fcf6232d2419df6
Marcelo Schlemper <marcelo63466446@alunos.sc.senac.br>
2026-04-08 15:52:08 -0300

    exercicios arrays

---
+++ src/Aula7.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class Aula7 {
+
+	public static void main(String[] args) {
+
+    //exemplo();
+	//exercicio();
+	exercicio2();
+		
+	}
+	static void exemplo() {
+			String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		System.out.println(cars[0]);
+		System.out.println(cars[2]);
+		
+		cars[2] = "chevrolet";
+		System.out.println(cars[2]);
+		
+		System.out.println("");
+		
+		for(int i = 0; i < cars.length; i++) {
+			System.out.println(i +  " -> " +cars[i]);
+			
+		}
+		
+		System.out.println("");
+		int x = 0;
+		while(x < cars.length) {
+			System.out.println(x + " -> " + cars[x]);
+			x++;
+			
+		} 
+		
+		//nao indexado - interativo
+		System.out.println("");
+		for(String c : cars ) { 
+			System.out.println(c);
+		}
+			
+			
+		
+		
+		}
+    static void exercicio() {
+    	 Scanner sc = new Scanner(System.in);
+		 int[] numeros = new int[5];
+		   
+		 for (int i = 0; i < 5; i++) {
+    	 System.out.print("digite um numero para a posiçao" + i + ":");
+		 numeros[i] =  sc.nextInt();
+		 }
+		 
+		//MOSTRANDO OS NUMEROS 
+		 System.out.println("/nNumeros dobrados");
+		 for(int i = 0; i < 5; i++) {
+			 int dobro = numeros[i] * 2;
+			 System.out.println("posiçao " + i + ": " + dobro);
+		 }
+		 sc.close();
+		 }
+    
+    static void exercicio2() {
+    	Scanner sc = new Scanner(System.in);
+    	int[] numeros = new int[10];
+		 
+		 for (int i = 0; i < 10; i++) {
+	    	 System.out.print("digite um numero para a posiçao" + i + ":");
+		 numeros[i] =  sc.nextInt();
+		 }
+		 for (int i = 0; i < 10; i++) {
+		    if (numeros[i] % 2 == 0) {
+			 System.out.println("o numero " + numeros[i] + " é par");
+		 } else {
+			 System.out.println("o numero " +  numeros[i] + " é impar");
+		 }
+		 
+		 }
+		 
+		   
+    }
+    
+	}  
+	
+
+	
+
+
+
