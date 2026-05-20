e4b4400c9670dce8b94c7416936b409d36613d67
Marcelo Schlemper <ma09.schlemper@gmail.com>
2026-05-20 14:23:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+
+
 const pessoas =
     JSON.parse(localStorage.getItem('clientes'))
     || [];
 
-const botao =
+const tabela =
+    document.getElementById('tabelaClientes');
+
+
+// Cadastrar
+
+const botaoCadastrar =
     document.getElementById('cadastrar');
 
-botao.addEventListener('click', () => {
+botaoCadastrar.addEventListener('click', () => {
 
     const nome =
         document.getElementById('nome').value;
@@ -16,15 +24,17 @@ botao.addEventListener('click', () => {
     const telefone =
         document.getElementById('telefone').value;
 
-    
-    if(nome === '' || email === '' || telefone === ''){
+    if(
+        nome === '' ||
+        email === '' ||
+        telefone === ''
+    ){
 
         alert('Preencha todos os campos');
 
         return;
     }
 
-    // objeto do cliente
     const pessoa = {
 
         nome,
@@ -33,33 +43,46 @@ botao.addEventListener('click', () => {
 
     };
 
-    // adiciona no array
     pessoas.push(pessoa);
 
-    // salva no localStorage
     localStorage.setItem(
         'clientes',
         JSON.stringify(pessoas)
     );
 
-    // limpa os inputs
+
+    // verifica se a tabela existe
+
+    if(tabela){
+
+        tabela.innerHTML += `
+        
+            <tr>
+                <td>${pessoa.nome}</td>
+                <td>${pessoa.email}</td>
+                <td>${pessoa.telefone}</td>
+            </tr>
+
+        `;
+
+    }
+
     document.getElementById('nome').value = '';
 
     document.getElementById('email').value = '';
 
     document.getElementById('telefone').value = '';
 
-    alert('Cliente cadastrado com sucesso');
-
 });
 
 
-// botão para ir para a página de cadastros
 
-const botaoSalvos =
+// mostrar cadastros
+
+const botaoMostrar =
     document.getElementById('salvos');
 
-botaoSalvos.addEventListener('click', () => {
+botaoMostrar.addEventListener('click', () => {
 
     window.location.href =
         'cadastros.html';
@@ -67,22 +90,9 @@ botaoSalvos.addEventListener('click', () => {
 });
 
 
-// limpar apenas a tabela
-
-const botaoLimparTabela =
-    document.getElementById('limparTabela');
-
-botaoLimparTabela.addEventListener('click', () => {
-
-    const tabela =
-        document.getElementById('tabelaClientes');
-
-    tabela.innerHTML = '';
-
-});
 
 
-// apagar todos os cadastros
+// limpar cadastros
 
 const botaoLimparCadastros =
     document.getElementById('limparCadastros');
@@ -92,5 +102,7 @@ botaoLimparCadastros.addEventListener('click', () => {
     localStorage.removeItem('clientes');
 
     alert('Cadastros apagados');
+
+    location.reload();
 
 });
